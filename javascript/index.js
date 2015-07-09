2b85d5f9371012e280ebfaa51e1f94ae3d427db8
Duncan <Duncan.David@gmail.com>
2015-07-09 12:26:56 -0700

    change button to a, simplification and cleanup of css

--- javascript/index.js
+++ javascript/index.js
@@ -4,15 +4,6 @@ $(document).ready(function(){
 			$("body").toggleClass("font1");
 			$("body").toggleClass("font2");
 		});
-		$(".mainbut").on("click", function(e){
-			e.preventDefault();
-			window.location = $(this).attr("data");
-		});
-		//temporary fix
-		$("#slide2").on("click", function(e){
-			e.preventDefault();
-			window.location = "https://DDunc.github.io/Sunk"
-		});
 });
 
 
