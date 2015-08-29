c7ce3fd09d46d55dddeb1bad612c813e3b657866
Duncan <Duncan.David@gmail.com>
2015-08-29 10:08:05 -0700

    revert to old version

---
+++ praxisbjj/bjjquiz.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+	var quizQ = [];
+	function QuestionMake(qtext){
+		this.qtext = qtext;
+		}
+		quizQ.push(this)
+	}
+
+	var question1 = ["Go Limp", "Do a flying triangle", "Tap out", "Hip escape (correct)"]
+
+	})
+
+	if (item.indexOf("(correct)") > -1){
+		item.replace("(correct)", "");
+	}
+	$("")
+
+
+
+})
